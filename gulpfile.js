aa402a45cce7a64ee72a238dba000e07fbedd414
Karol P <karol.wsnyc@gmail.com>
2015-09-23 11:43:11 +0200

    Update GulpJS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,40 @@
 /**
  *  Configuration
  */
-var SKIN_DIR    = './skin/frontend/rwd/par';
-var APP_DIR     = './app/design/frontend/rwd/wsnyc';
-var HOST_URL    = 'http://YOUR_HOST_URL.local/';   // e.g.: http://magento-store-1.local
+var SKIN_DIR = './skin/frontend/rwd/parfienczyk';
+var APP_DIR = './app/design/frontend/rwd/parfienczyk';
+var HOST_URL = 'http://YOUR_LOCAL_VHOST.LINK/';   // e.g.: http://magento-store-1.local
+
+// IMPORTANT: we don't why but gulp works very slow when domain is: http://some_address.local
+// better solution and faster reload is when domain is: http://some_address.dev
+
 
 /**
  * Required modules (pulled in from package.json file by doing
  * 'npm install' from the command line)
  */
-
-var autoprefixer = require('autoprefixer-core');
-var browserify = require('gulp-browserify');
-var browserSync = require('browser-sync');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var imagemin = require('gulp-imagemin');
-var jshint = require('gulp-jshint');
-var minifyCSS = require('gulp-minify-css');
-var postcss = require('gulp-postcss');
-var reload = browserSync.reload;
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
+var autoprefixer    = require('autoprefixer');
+var browserify      = require('gulp-browserify');
+var imagemin        = require('gulp-imagemin');
+var jshint          = require('gulp-jshint');
+var postcss         = require('gulp-postcss');
+var rename          = require('gulp-rename');
+var sass            = require('gulp-sass');
+var uglify          = require('gulp-uglify');
+var gulp            = require('gulp');
+var browserSync     = require('browser-sync').create();
+var minifyCss       = require('gulp-minify-css');
+var sourcemaps      = require('gulp-sourcemaps');
+var concat          = require('gulp-concat');
+var plumber         = require('gulp-plumber');
 
 
 
 // Launch web server
-gulp.task('connect', function () {
+gulp.task('serve', function () {
     // Serve files from the root of this project
-    browserSync({
-        notify: false,
+    browserSync.init({
+        notify: true,
         proxy: HOST_URL
         // logLevel: 'debug',
         // logConnections: true,
@@ -38,71 +42,106 @@ gulp.task('connect', function () {
         //    baseDir: "./"
         //}
     });
-});
 
-
-// Static server
-// use default task to launch BrowserSync and watch files
-gulp.task('serve', ['sass', 'connect'], function () {
-
-    // Set up our watcher tasks. This in turn will fire off the associated
-    // tasks when it sees a file has been changed.
     gulp.watch(SKIN_DIR + '/scss/**/*.scss', ['sass']);
     gulp.watch(SKIN_DIR + '/js/*.js', ['js']);
-    gulp.watch(SKIN_DIR + '/images/*.*', ['images']);
 
     // add browserSync.reload to the tasks array to make
     // all browsers reload after tasks are complete.
-    gulp.watch(APP_DIR + '/template/**/*.phtml').on('change', reload);
-    gulp.watch(APP_DIR + '/layout/**/*.xml').on('change', reload);
+    gulp.watch(APP_DIR + '/template/**/*.phtml').on('change', browserSync.reload);
+    gulp.watch(APP_DIR + '/layout/**/*.xml').on('change', browserSync.reload);
 });
 
 
 // SASS (minify + autoprefixer)
 gulp.task('sass', function () {
     gulp.src(SKIN_DIR + '/scss/**/*.scss')
-        .pipe(sass()).on('error', gutil.log)
-        .pipe(minifyCSS())
-        .pipe(postcss([autoprefixer({browsers: ['last 10 version']})])).on('error', gutil.log)
-        .pipe(gulp.dest(SKIN_DIR + '/css')).on('error', gutil.log)
-        .pipe(reload({stream: true}));
+        .pipe(plumber())
+        .pipe(sass.sync())
+        .pipe(postcss([autoprefixer({browsers: ['last 10 version']})]))
+        .pipe(sourcemaps.init())
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write())
+        .pipe(browserSync.reload({stream: true}))
+        .pipe(gulp.dest(SKIN_DIR + '/css'));
 });
 
 
 // Pass our js through browserify
 gulp.task('js', ['jshint'], function () {
+
+    var scripts = [
+        './bower_components/jquery/dist/js/jquery.min.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        //'src/js/script.js'
+    ];
+
+    gulp.src(scripts)
+        .pipe(plumber())
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(SKIN_DIR + '/js'));
+
+
     gulp.src([
-            SKIN_DIR + '/js/custom.js',     // e.g.: *.js, **/*.js
-            '!' + SKIN_DIR + '/js/*.min.js'
-        ])
-        //.pipe(browserify()).on('error', gutil.log)
+        SKIN_DIR + '/js/custom.js',     // e.g.: *.js, **/*.js
+        '!' + SKIN_DIR + '/js/*.min.js'
+    ])
+        .pipe(plumber())
+        //.pipe(browserify())
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(SKIN_DIR + '/js/'))
-        .pipe(reload({stream:true}));
+        .pipe(browserSync.reload({stream: true}));
 });
-
 
 // Configure the JShint task
 gulp.task('jshint', function () {
-    gulp.src([SKIN_DIR + '/js/*.js', '!' + SKIN_DIR + '/js/*.min.js'])
+    gulp.src([
+        SKIN_DIR + '/js/custom.js',
+        '!' + SKIN_DIR + '/js/custom*.min.js'
+    ])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 
+// Fonts
+gulp.task('fonts', function() {
+    return gulp
+        .src([
+            'bower_components/bootstrap/fonts/**/*',
+            'bower_components/font-awesome/fonts/**/*'
+        ])
+        .pipe(gulp.dest('fonts'));
+});
+
 // Optimize all images
-gulp.task('images', function () {
+gulp.task('image', function () {
     gulp.src(SKIN_DIR + '/images/**/*.{png,jpg,gif}')
+        .pipe(plumber())
         .pipe(imagemin({optimizationLevel: 5}))
         .pipe(gulp.dest(SKIN_DIR + '/images'));
 });
 
+// Watch
+gulp.task('watch', function () {
+
+    // Watch .scss files
+    gulp.watch(SKIN_DIR + '/scss/**/*.scss', ['sass']);
+
+    // Watch .js files
+    gulp.watch(SKIN_DIR + '/js/*.js', ['js']);
+
+    // Watch image files
+    gulp.watch(SKIN_DIR + '/images/**/*', ['images']);
+});
 
 // Initialization
-gulp.task('init', ['sass', 'jshint', 'js']);
+gulp.task('dev', ['sass', 'js']);
+gulp.task('prod', ['sass', 'js', 'image', 'fonts']);
 
-gulp.task('default', function() {
-    console.log('To start using the package, use "$ gulp init"');
+gulp.task('default', function () {
+    console.log('To start using the package, use "$ gulp prod"');
     console.log('To resume Sass\'s "watch" tasks, use "$ gulp serve"');
 });
